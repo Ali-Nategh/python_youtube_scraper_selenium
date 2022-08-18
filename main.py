37d318ef73413f6c8dd72514e2e4c37e2278f81a
Ali Nategh <alinategh005@gmail.com>
2022-08-18 17:13:15 +0430

    refactor: description

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ import xlsxwriter
 # using IDs and NAMES and TAGs instead is way more stable than just using indexes of the js info tag
 # the only thing it is missing is the channel subscribers count wich is a challenge here aswell,
 # I might update it later and add this feature to it.
+#
+# Also included some test samples made with this script in the github repository.
+#
 # Hope you can use this and have fun!
 
 
